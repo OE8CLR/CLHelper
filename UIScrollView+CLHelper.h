31a981f9753a3b78d8f4f19fcf2ad29dc9c999f0
Christoph Lückler <christoph.lueckler@ut11.net>
2014-05-20 14:00:11 +0200

    Add UIScrollView categorie

---
+++ UIScrollView+CLHelper.h
@@ -0,0 +1,17 @@
+//
+//  UIScrollView+CLHelper.h
+//  MySMS-Messenger
+//
+//  Created by Christoph Lückler on 20.05.14.
+//  Copyright (c) 2014 sms.at mobile internet services gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (CLHelper)
+
+- (BOOL)isAtTop;
+
+- (BOOL)isAtBottom;
+
+@end
---
+++ UIScrollView+CLHelper.m
@@ -0,0 +1,34 @@
+//
+//  UIScrollView+CLHelper.m
+//  MySMS-Messenger
+//
+//  Created by Christoph Lückler on 20.05.14.
+//  Copyright (c) 2014 sms.at mobile internet services gmbh. All rights reserved.
+//
+
+#import "UIScrollView+CLHelper.h"
+
+@implementation UIScrollView (CLHelper)
+
+- (BOOL)isAtTop {
+    return (self.contentOffset.y <= [self verticalOffsetForTop]);
+}
+
+- (BOOL)isAtBottom {
+    return (self.contentOffset.y >= [self verticalOffsetForBottom]);
+}
+
+- (CGFloat)verticalOffsetForTop {
+    CGFloat topInset = self.contentInset.top;
+    return -topInset;
+}
+
+- (CGFloat)verticalOffsetForBottom {
+    CGFloat scrollViewHeight = self.bounds.size.height;
+    CGFloat scrollContentSizeHeight = self.contentSize.height;
+    CGFloat bottomInset = self.contentInset.bottom;
+    CGFloat scrollViewBottomOffset = scrollContentSizeHeight + bottomInset - scrollViewHeight;
+    return scrollViewBottomOffset;
+}
+
+@end
